4b3369d117b64826e1489139c502fa885df12754
Robin Watson <drumsrobin@gmail.com>
2018-09-26 21:12:30 -0400

    cleaned up comments

--- project1-1/app.js
+++ project1-1/app.js
@@ -1,4 +1,3 @@
-
 const app = {};
 
 //Reference all password fields
@@ -7,33 +6,51 @@ app.currentPasswordInput = document.querySelector('#current-pw');
 app.newPasswordInput = document.querySelector('#new-pw');
 app.document = document;
 
-// listen for a click on entire document - using event
-app.document.addEventListener('click', function (e) {
-  //if checkbox is selected
+
+app.document.addEventListener('click', function(e) {
   if (e.target.id === 'show-password' && e.target.type === 'checkbox') {
-    // And, the input type  of password is password
     if(app.loginPasswordInput.getAttribute('type') === 'password') {
-      // switch the type to text so it can be read
       app.loginPasswordInput.setAttribute('type', 'text');
     } else {
-      // else, when button is clicked again, set it back to password
       app.loginPasswordInput.setAttribute('type', 'password');
     }
   }
-  // if the checkbox is selected
+
   if (e.target.id === 'show-passwords' && e.target.type === 'checkbox') {
-    //and the input type is assigned to password for both
-    if(app.currentPasswordInput.getAttribute('type') === 'password' && app.newPasswordInput.getAttribute('type') === 'password') {
-      // set it to type so you can see it
-      app.currentPasswordInput.setAttribute('type', 'text');
+    if(app.currentPasswordInput.getAttribute('type') === 'password' &&
+    app.newPasswordInput.getAttribute('type') === 'password') {
+      app.currentPasswordInput.setAttribute('type', 'text')
       app.newPasswordInput.setAttribute('type', 'text');
     } else {
-      // else, change them both back on click again
-      app.currentPasswordInput.setAttribute('type', 'password');
+      app.currentPasswordInput.setAttribute('type', 'password')
       app.newPasswordInput.setAttribute('type', 'password');
     }
   }
 });
+
+
+  
+
+
+
+  
+
+
+
+
+
+
+
+ 
+  
+
+
+
+
+
+
+
+
 
 
 
