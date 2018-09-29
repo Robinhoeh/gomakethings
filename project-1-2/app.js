383f35675b8ced515eed87c4e61930f93d18fd27
Robin Watson <drumsrobin@gmail.com>
2018-09-29 09:15:41 -0400

    cleaned up commments, got rid of arrow function

--- project-1-2/app.js
+++ project-1-2/app.js
@@ -1,16 +1,3 @@
-// (temp - 32) * 5/9 Farenhiet to celcius
-// (temp * 9/5) + 32 Celcius to Farenheit
-
-/*
-Detect when the value of one of the two inputs is changed.
-Do some math and then update the other value.
-*/
-
-/*
-  click on input an enter number
-  when you click on first input, it will instantly update and covert the other number
-*/
-
 // Namespace
 const app = {}
 // Reference elements
@@ -18,29 +5,17 @@ app.farenheitInput = document.querySelector('#farenheit');
 app.celciusInput = document.querySelector('#celcius');
 app.document = document;
 
-// Convert Farenheit -> Celcius V.versa
-
-
-
-
 // Listen for input changes
 function inputHandler(e) {
   if(e.target.id === 'farenheit' && e.target.type === 'number') {
-    //take number inputed by user and convert it to celcius
-    // then make that the value of the celcius input
     app.userFarenheitInput = Math.round((app.farenheitInput.value - 32) * 5 / 9);
-    // set the value of celcius to the converted value of farenheit
     app.celciusInput.value = app.userFarenheitInput;
   } else {
       e.target.id === 'celcius' && e.target.type === 'number'
-    //take number inputed by user and convert it to farenheit
-    //then make that the value of farenheit
       app.userCelciusInput = Math.round((app.celciusInput.value * 9 / 5) + 32);
-      //set the value of farenheit to the converted value of celcius
       app.farenheitInput.value = app.userCelciusInput;
   }
 }
-
 app.document.addEventListener('input', inputHandler, false);
 
 
