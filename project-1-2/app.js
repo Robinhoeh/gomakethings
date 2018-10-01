7e369555f9213043a9c6ff96625edb7b87b4e7aa
Robin Watson <drumsrobin@gmail.com>
2018-10-01 14:40:30 -0400

    fixed syntax erorr, logic was correct

--- project-1-2/app.js
+++ project-1-2/app.js
@@ -7,12 +7,18 @@ app.document = document.documentElement;
 
 // Listen for input changes
 app.document.addEventListener('input', function (e) {
+  // if value of user input < 1, quit
   if (e.target.value.length < 1) return;
   //if target matches id of farenheit or is a number
   if(e.target.id === 'js-farenheit' && e.target.type === 'number') {
+    // the value of F converted to Celcius, display dynamic value in both inputs
     app.celciusInput.value = farenheitToCelcius(e.target.value)
-  } else (e.target.id === 'js-celcius' && e.target.type === 'number');
-      app.farenheitInput.value = celciusToFarenheit(e.target.value);
+  } else {
+    // if input is C
+    e.target.id === 'js-celcius' && e.target.type === 'number';
+    // the value of C is converted and displayed dynamically in both values
+    app.farenheitInput.value = celciusToFarenheit(e.target.value);
+  }
 }, false);
 
 
