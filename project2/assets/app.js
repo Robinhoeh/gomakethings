ebac281f569464007e3368e50db14f04eb211933
Robin Watson <drumsrobin@gmail.com>
2018-10-02 18:47:16 -0400

    looped through posts - attempting to display to page

--- project2/assets/app.js
+++ project2/assets/app.js
@@ -5,6 +5,11 @@ What should the app do if the API fails to return a quote?
 How should clicking the More Ron button work, and what can you do to avoid writing the same quote twice?
 */
 
+// when somoneclick on the button
+// make a call to the API
+// display the quote on the page
+// if request fails, display fail message
+// stop if from return same quote twice
 
 // Reference to buttons
 const app = {}
@@ -14,24 +19,16 @@ app.secondHeader = document.querySelector('.second-header');
 app.errorHeader = document.querySelector('.error-header');
 app.document = document.querySelector('document');
 
-// when somoneclick on the button
-// make a call to the API
-// display the quote on the page
-// if request fails, display fail message
-// stop if from return same quote twice
-
-
 
 const makeRequest = function (url, method, success, failure, always) {
-
   // Make sure a URL and method were provided
   if (!url || !method) return;
-
   // Set up our HTTP request
   const xhr = new XMLHttpRequest();
-
   // Setup our listener to process request state changes
   xhr.onreadystatechange = function () {
+     // Only run if the request is complete
+    if (xhr.readyState !== 4) return;
     // Process our return data
     if (xhr.status >= 200 && xhr.status < 300) {
       // Run the success callback
@@ -53,20 +50,23 @@ const makeRequest = function (url, method, success, failure, always) {
   // Defaults to GET
   xhr.open(method, url);
   xhr.send();
-
 };
 
 // Get a list of posts
-makeRequest('http://ron-swanson-quotes.herokuapp.com/v2/quotes', 'GET', function (posts) {
+function makeApiCall () {
+  makeRequest('http://ron-swanson-quotes.herokuapp.com/v2/quotes','GET', function (posts) {
     posts.forEach(function (post) {
-        console.log(post);
-        //Disaply data to the DOM
-        const listUl = document.querySelector('.list-header');
-        const postItem = document.createElement('li');
-        postItem.textContent = `${post.id} - ${post.title}`;
-        listUl.appendChild(postItem);
+      console.log(post);
+      //Disaply data to the DOM
+      const listUl = document.querySelector('.list-header');
+      const postItem = document.createElement('li');
+      postItem.textContent = `${post}`;
+      console.log(postItem);
+      listUl.appendChild(postItem);
     });
-});
+  });
+}
 
+app.apiCallButton.addEventListener('click', makeApiCall);
 
 
