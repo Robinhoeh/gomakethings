3a73acca8615c6e000c84d3d8f10d1243efc02a2
Robin Watson <drumsrobin@gmail.com>
2018-10-10 17:58:16 -0400

    updated rendering section

--- project-2-2/assets/app.js
+++ project-2-2/assets/app.js
@@ -1,19 +1,17 @@
 // Display the top stories from the NYTimes
 // Pick 3 - 5 sections and display 3 - 5 top stories
 
-/*
-Variables
-*/
+
+// Set variables
 const newsApp = {};
+newsApp.app = document.querySelector('#app');
 newsApp.apiKey = '68331c5281b443f89dd1f073aab9aa54';
 newsApp.sections = ['technology','science', 'magazine'];
 newsApp.articleCount = 3;
 
-
-
 //Sanatize HTML
 newsApp.sanitizeHTML = function (str) {
-   newsApp.temp = document.createElement('div');
+  newsApp.temp = document.createElement('div');
   newsApp.temp.textContent = str;
   return newsApp.temp.innerHTML;
 };
@@ -21,15 +19,15 @@ newsApp.sanitizeHTML = function (str) {
 newsApp.renderArticles = function(articles) {
   newsApp.content = '';
   articles.forEach( article => {
-    newsApp.content += `
-      <li>
-        <strong><a href="`${newsApp.sanatizeHTML(article.url)}${newsApp.sanatizeHTML(article.title)}`"</a></strong><br>
-        <span class="text-muted text-small">${newsApp.sanatizeHTML(article.byline)}</span><br>
-        <span class="text-small>${newsApp.sanatizeHTML(article.abstract)}</span>
-      </li>
-    `
-  })
-}
+    newsApp.content +=
+    `<li>
+      <strong><a href=`${newsApp.sanatizeHTML(article.url)}${newsApp.sanatizeHTML(article.title)}`</a></strong><br>
+      <span class="text-muted text-small">${newsApp.sanatizeHTML(article.byline)}</span><br>
+      <span class="text-small>${newsApp.sanatizeHTML(article.abstract)}</span>
+      </li>`
+  });
+};
+
 
 
 //Render section helper function
@@ -41,8 +39,7 @@ newsApp.renderSection = function(articles, title) {
     <h2 class="title-case">${title}</h2>
     <ol>${newsApp.renderedArticles(articles)}</ol>
   `;
-  newsApp.app = document.querySelector('#app');
-  newApp.app.append(newsApp.section);
+  newsApp.app.append(newsApp.section);
 }
 
 
