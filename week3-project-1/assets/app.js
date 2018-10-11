66ddd34cde05c3a2670adbd525cf373c5aae8916
Robin Watson <drumsrobin@gmail.com>
2018-10-11 19:25:22 -0400

    added some class names and made some pseudo code

---
+++ week3-project-1/assets/app.js
@@ -0,0 +1,21 @@
+/*
+How do you figure out how many words are in the text?
+How do you deal with things like double spaces after periods, or someone who accidentally hits the space bar a few times (two? three?) between words?
+
+As the user types, the .word-count and .character-count elements should update to show the number of words and characters, respectively.
+*/
+
+// Initiate reference to DOM elements that get updated
+// Listen for user input in text area
+// Constantly check length of word every keydown/keyup
+// Update the word count variable
+// Constantly check length of entire character count
+// Update the character count variable
+
+// Variables
+const wordApp = {};
+wordApp.document.querySelector('.app');
+wordApp.document.querySelector('.js-word-count');
+wordApp.document.querySelector('.js-character-count')
+
+
