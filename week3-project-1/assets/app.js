c6a6fdfe3470fd3eadae92ea4c3996b02937a436
Robin Watson <drumsrobin@gmail.com>
2018-10-16 19:53:51 -0400

    cleaned up code, added focus

--- week3-project-1/assets/app.js
+++ week3-project-1/assets/app.js
@@ -5,19 +5,14 @@ How do you deal with things like double spaces after periods, or someone who acc
 As the user types, the .word-count and .character-count elements should update to show the number of words and characters, respectively.
 */
 
-
-// Constantly check length of entire character count
-// Update the character count variable
-
-// check for space after any letter
-// space implies character count
-
 // Variables
 const wordApp = {};
-wordApp.app = document.querySelector('.app');
 wordApp.wordCountEl = document.querySelector('.js-word-count');
 wordApp.charLength = document.querySelector('.js-character-count');
 wordApp.textArea = document.querySelector('.js-text-area');
+
+// Autofocus on textarea
+wordApp.textArea.focus();
 
 //Check amount of characters, update the DOM
 wordApp.amountOfChar = e =>  {
@@ -30,7 +25,11 @@ wordApp.textArea.addEventListener('keyup', wordApp.amountOfChar, false);
 //Check word count
 wordApp.wordCount = () => {
   wordApp.regex = /\s+/gi;
-  wordApp.wordCountEl.textContent = wordApp.textArea.value.trim().replace(wordApp.regex, ' ').split(' ').length;
+  wordApp.wordCountEl.textContent = wordApp.textArea.value
+    .trim()//remove all white space
+    .replace(wordApp.regex, ' ')//replace every character with a space
+    .split(' ')//
+    .length;//
 }
 
 wordApp.textArea.addEventListener('keyup', wordApp.wordCount, false);
