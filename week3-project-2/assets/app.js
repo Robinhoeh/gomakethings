6f43ca96ef5cd971baa015f5920c2437b1fd18d7
Robin Watson <drumsrobin@gmail.com>
2018-10-16 20:20:44 -0400

    setup files and folders for project 2

---
+++ week3-project-2/assets/app.js
@@ -0,0 +1,15 @@
+// The monsters and socks
+var monsters = [
+  'sock!',
+  'monster1.svg',
+  'monster2.svg',
+  'monster3.svg',
+  'monster4.svg',
+  'monster5.svg',
+  'monster6.svg',
+  'monster7.svg',
+  'monster8.svg',
+  'monster9.svg',
+  'monster10.svg',
+  'monster11.svg'
+];
